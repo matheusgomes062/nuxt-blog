7e7ef0662b7305b8f93ee021c20520064265a0fd
Matheus Gomes <matheusgomes062@gmail.com>
2024-12-04 08:33:34 -0300

    chore: bootstrapping the project

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  css: ['/assets/css/main.css'],
+  ssr: true,
+  experimental: {
+      payloadExtraction: false
+  },
+  router: {
+      options: {
+          strict: false
+      }
+  },
+  sourcemap: false,
+  content: {
+    // Configuring code highlighting
+    // https://content.nuxtjs.org/api/configuration
+    highlight: {
+        theme: 'github-dark',
+        // Define languages you expect to use
+        preload: ['java','javascript']
+    },
+    markdown: {
+        // Configuring external link processing
+        // https://github.com/rehypejs/rehype-external-links
+        rehypePlugins: [
+            [
+                'rehype-external-links',
+                {
+                    target: '_blank',
+                    rel: 'noopener noreferer'
+                }
+            ]
+        ]
+    }
+  }
+})
